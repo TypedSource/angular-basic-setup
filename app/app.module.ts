0b60200a088416f32bb068619634e823843bc13e
Maik Tizziani <typedsource@gmail.com>
2017-06-18 23:45:36 +0200

    implement ApiService and HttpModule

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,14 @@
 import {NgModule} from "@angular/core";
 import {AppComponent} from './app.component';
 import {BrowserModule} from "@angular/platform-browser";
+import {ApiService} from "./api.service";
+import {HttpModule} from "@angular/http";
 
 
 @NgModule({
-    imports: [BrowserModule],
+    imports: [BrowserModule, HttpModule],
     declarations:[AppComponent],
-    bootstrap: [AppComponent]
+    bootstrap: [AppComponent],
+    providers: [ApiService]
 })
 export class AppModule {}
