0212fd3a2ac787f7648dc7bf21a27d59f7902e64
Maik Tizziani <typedsource@gmail.com>
2017-06-18 23:50:49 +0200

    changes from main.ts to app.ts

--- system.config.js
+++ system.config.js
@@ -22,7 +22,7 @@
         },
         // packages tells the System loader how to load when no filename and/or no extension
         packages: {
-            app: { main: './main.js', defaultExtension: 'js' },
+            app: { main: './app.js', defaultExtension: 'js' },
             rxjs: { defaultExtension: 'js' }
         }
     });
