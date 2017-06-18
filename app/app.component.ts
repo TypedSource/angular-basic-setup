a68d6d2a157a1fbbf9fa5364798e92587a98d8bc
Maik Tizziani <typedsource@gmail.com>
2017-06-18 23:48:38 +0200

    implements api service and a sample for using jquery

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,47 @@
-import {Component} from "@angular/core";
+import {Component, ElementRef, AfterViewInit} from "@angular/core";
+import {ApiService} from "./api.service";
 
 @Component({
     selector: 'app',
-    templateUrl: './app/app.template.html',
-    styleUrls: ['./app/app.style.css']
+    templateUrl: './app/app.component.html',
+    styleUrls: ['./app/app.component.css']
 })
-export class AppComponent {
-    constructor(){
-        console.log('AppComponent created');
+export class AppComponent implements AfterViewInit{
+    resText: string;
+    resJson: string;
+    author: string = 'Maik Tizziani';
+    copyright: string = `&copy; 2017 by ${this.author}`;
+
+    constructor(private el: ElementRef, private apiService: ApiService){
+        apiService.getJsonFile('test').subscribe(
+            data => this.resJson = JSON.stringify(data, null, 2),
+            error => alert('error on loading'),
+            () =>  this.requestComplete()
+        );
+
+        apiService.getTextFile('test').subscribe(
+            data => this.resText = data,
+            error => alert('error on loading'),
+            () => this.requestComplete()
+        )
     }
+
+
+    requestComplete(){
+        console.log('request complete');
+        console.log(arguments);
+    }
+
+    ngAfterViewInit(): void {
+        let node = $(this.el.nativeElement);
+        console.log('view init complete');
+        console.log($);
+        console.log(typeof node);
+        console.log(node instanceof $);
+        console.log(node);
+        console.log(typeof node[0]);
+        console.log(node[0] instanceof $);
+        console.log(node[0]);
+    }
+
 }
